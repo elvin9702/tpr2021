7f1d26ac2f88e5b993aec84fc2ccab9e237c9bae
elvin9702 <elvinvergara8@gmail.com>
2021-08-30 00:41:02 -0400

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
+
+route::get('cursos', [CursoController::class, 'index']);
+//cursos/html5
+
+Route::get('cursos/create', [CursoController::class, 'create']);
+
+route::get('cursos/{curso}', [CursoController::class, 'show']);
+
+
